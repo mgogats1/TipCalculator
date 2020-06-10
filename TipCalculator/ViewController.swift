fad71bb21009f597a3a448af278628151121cc35
Max Gogats <mgogats@gmail.com>
2020-06-10 18:28:19 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Max Gogats on 6/10/20.
+//  Copyright © 2020 MeeksDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
